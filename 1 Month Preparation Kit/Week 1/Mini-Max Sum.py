925d22fbb4877d9e9d70aeb7f84a852e579c3911
EnzoPonce <enzoponce21@gmail.com>
2022-08-22 16:38:02 -0300

    Challenge

---
+++ 1 Month Preparation Kit/Week 1/Mini-Max Sum.py
@@ -0,0 +1,17 @@
+arr = [5,5,5,5,5]
+
+def miniMaxSum(arr):
+    # Write your code here
+    suma = 0
+    suma2 = 0
+    maximo = max(arr)
+    minimo = min(arr)
+    for element in arr:
+        suma = sum(arr)
+        suma2 = sum(arr)
+    suma = suma-maximo
+    suma2 = suma2 - minimo 
+    print(suma, suma2)    
+
+miniMaxSum(arr)
+
